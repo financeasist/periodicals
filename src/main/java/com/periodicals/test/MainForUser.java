87f5aa6a6991309965265edd3ce78ff7d2ac05b3
Roman Grupskyi <financeasist@gmail.com>
2016-11-03 20:29:38 +0200

    modified pom.xml

--- src/main/java/com/periodicals/test/MainForUser.java
+++ src/main/java/com/periodicals/test/MainForUser.java
@@ -11,9 +11,9 @@ public class MainForUser {
 	public static void main(String[] args) throws SQLException {
 	
 	UserDaoImpl userDao = new UserDaoImpl();
-	User user = new User("sadh@vcb.kom","hgfdfgh","Roman","null",Roles.GUEST,false);
-	userDao.create(user);
-//	userDao.deleteByEmail("sadhfj@vbcb.kom");
+	User user = new User("sadg@vcb.kom","hgfdfgh","Roman","null",Roles.GUEST,false);
+	//userDao.create(user);
+	userDao.deleteByEmail("sadhfj@vbcb.kom");
 	
 //	System.out.println(userDao.getByID(2));
 //	userDao.update_EmailANDPassword_ByID(user,2);
